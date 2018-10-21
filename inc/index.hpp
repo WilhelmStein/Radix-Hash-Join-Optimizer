0e86df43e83c81bff3cb933808bdf03cc87915de
billsioros <billsioros97@gmail.com>
2018-10-21 22:52:06 +0300

    Index implementation (Step 2)

---
+++ inc/index.hpp
@@ -0,0 +1,37 @@
+
+#pragma once
+
+#include "histhash.h"
+#include "types.hpp"
+#include "pair.hpp"
+#include <functional>
+
+class Index
+{
+    const Bucket& _data;
+    
+    chain_key_t * _chain;
+
+    bucket_key_t * _bucket;
+    bucket_size_t _bucketSize;
+
+    std::function<bucket_key_t(const Tuple&)> _hash;
+
+public:
+
+    using Result = nstd::pair<relation_size_t, relation_size_t>;
+
+    Index(const Bucket&);
+    ~Index();
+
+    const Bucket& data() const { return _data; }
+
+    const chain_key_t * chain() const  { return _chain; }
+
+    const bucket_key_t * bucket() const { return _bucket; }
+    bucket_size_t bucketSize() const { return _bsize; }
+
+    const std::function<bucket_key_t(const Tuple&)>& hash() const { return _hash; }
+
+    Result join(const Block&) const;
+};
