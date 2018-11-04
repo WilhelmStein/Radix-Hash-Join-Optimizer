c50fbb4f535f6bd2b6ed174403a6182b7190fa3e
billsioros <billsioros97@gmail.com>
2018-11-04 22:59:52 +0200

    Updated benchmark.hpp that returns result and counts time and cpu ticks

--- inc/benchmark.hpp
+++ inc/benchmark.hpp
@@ -8,17 +8,26 @@
 namespace utility
 {
     template <typename Task, typename ...Args>
-    double benchmark(Task&& task, Args&&... args)
+    auto benchmark(Task&& task, Args&&... args) -> typename std::result_of<Task(Args...)>::type
     {
-        auto beg = std::chrono::high_resolution_clock::now();
+        auto tbeg = std::chrono::high_resolution_clock::now();
+        auto cbeg = std::clock();
 
-        task(std::forward<Args>(args)...);
+        auto rv = task(std::forward<Args>(args)...);
 
-        auto end = std::chrono::high_resolution_clock::now();
-        
-        return static_cast<double>
+        auto tend = std::chrono::high_resolution_clock::now();
+        auto cend = std::clock();
+
+        const double tdiff = static_cast<double>
         (
-            std::chrono::duration_cast<std::chrono::microseconds>(end - beg).count()
+            std::chrono::duration_cast<std::chrono::microseconds>(tend - tbeg).count()
         ) / 1000.0;
+
+        const double cdiff = cend - cbeg;
+
+        std::cout << "milliseconds: " << tdiff << std::endl;
+        std::cout << "clock ticks: "  << cdiff << std::endl;
+
+        return rv;
     }
 }
