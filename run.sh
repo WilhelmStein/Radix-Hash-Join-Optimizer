0c8cfb50eecf232b5b971e9e2ca978bcba3889da
billsioros <billsioros97@gmail.com>
2018-11-04 22:58:42 +0200

    run.sh: a script to run various benchmarking sessions

---
+++ run.sh
@@ -0,0 +1,33 @@
+__CACHE_SIZE__=(4 8 16 32 64)
+
+if [ "$#" -lt 2 ]
+then
+    echo "Usage: $(basename "$0") [EXE] [OUT]"
+    exit 1
+fi
+
+exe="$1"; shift;
+out="$1"; shift;
+
+if [ -f "$out" ]
+then
+    read -p "Are you sure you want to overwrite $out: " answer
+    if [[ "$answer" != [yY] ]] && [[ "$answer" != [yY][eE][sS] ]]
+    then
+        exit 2
+    fi
+fi
+
+echo > "$out"
+
+defs=("__MEDIUM__" "__LARGE__")
+for def in "${defs[@]}"
+do
+    for ((pow = 4; pow <= 64; pow *= 2))
+    do
+        size=$((pow * 1024))
+        eval "./build.sh -r -x __BENCHMARK__ -x __SILENCE__ -x $def -g __CACHE_SIZE__=$size"
+
+        "$exe" >> "$out"
+    done
+done
