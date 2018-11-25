b9a422d34bf9e9e6cae307d78cfd74c9d93ea0cc
WilhelmStein <johnfreeman16561@gmail.com>
2018-11-25 21:02:14 +0200

    init test done except 1 mem leak

--- test/init_test.cpp
+++ test/init_test.cpp
@@ -18,40 +18,49 @@ using RHJ::Relation;
 
 struct Meta
 {
-    std::size_t rowSize, columnSize;
-
-    RHJ::Relation::Tuple * columns;
+    uint64_t rowSize, columnSize;
+    tuple_key_t** columns;
 } * meta;
 
-int main()
+
+int main(void)
 {
-    char* str = new char[PATH_MAX + 1];
-    size_t inputSize = 0;
+    utility::list<char*> paths;
+    char* str = nullptr;
 
     while(true) {
+        str = new char[PATH_MAX + 1];
         cin.getline(str, PATH_MAX);
+
         if (!strcmp(str, "Done"))
+        {
+            delete[] str;
             break;
-        inputSize++;
+        }
+        paths.push_back(str);
+    }
+    
+    if(!paths.size())
+    {
+        cerr<<"Error: no paths"<<endl;
+        return -1;
     }
 
-    cin.seekg(0);
-    
-    meta = new Meta[inputSize];
+    meta = new Meta[paths.size()];
 
     size_t currentFileNo = 0;
 
-    do {
+    while( !paths.empty() ) {
         // Get each file path
-        cin.getline(str, PATH_MAX);
-        
-
         FILE* inputFile = nullptr;
 
-        if( !( inputFile = fopen(str, "r") ) ){
-            cerr<<"Could not open file "<<str<<"\nErrorno: "<<errno<<endl;
+        if( !( inputFile = fopen(paths.back(), "r+b") ) ){
+            cerr<<"Could not open file "<<paths.back()<<"\nErrorno: "<<errno<<endl;
             return -1;
         }
+
+        delete[] paths.front();
+        paths.erase(paths.begin());
 
         // Get length of file
         fseek(inputFile, 0, SEEK_END);
@@ -59,27 +68,41 @@ int main()
         fseek(inputFile, 0, SEEK_END);
 
         // Load whole file in memory
-        void* p = mmap(NULL, file_size, PROT_READ | PROT_WRITE, MAP_SHARED, fileno(inputFile), 0);
+        void* p = mmap(NULL, file_size, PROT_READ | PROT_WRITE , MAP_SHARED, fileno(inputFile), 0);
+        if( p == MAP_FAILED )
+        {
+            cerr<<"Error: Mmap failed.\nErrorno: "<<errno<<endl;
+
+            return -1;
+        }
+
         fclose(inputFile);
 
-        uint64_t tuples(0UL), columns(0UL);
-
         // Copy Metadata
-        memmove( (void *) &tuples, p, sizeof(uint64_t));
+        memmove( (void *) &meta[currentFileNo].rowSize, p, sizeof(uint64_t));
         p = (char*)p + sizeof(uint64_t);
-        memmove( (void *) &columns, p, sizeof(uint64_t));
+        memmove( (void *) &meta[currentFileNo].columnSize, p, sizeof(uint64_t));
         p = (char*)p + sizeof(uint64_t);
 
-        // Create Index        
-        for(uint64_t j = 0UL; j < columns; j++) {
-            memmove(&meta[currentFileNo].columns[j], (void*) ((char*)p + j*tuples), sizeof(uint64_t));
+        // Create Index
+        meta[currentFileNo].columns = new tuple_key_t*[meta[currentFileNo].columnSize]; 
+        for(uint64_t j = 0UL; j < meta[currentFileNo].columnSize; j++) {
+            meta[currentFileNo].columns[j] = ((tuple_key_t*)p + j*meta[currentFileNo].rowSize);
         }
         
         currentFileNo++;
-    } while( true );
+    }
 
+    for(size_t i = 0; i < currentFileNo; i++) {
+        size_t ret = munmap( (void*) (  *meta[i].columns - 2 ) , (meta[i].columnSize * meta[i].rowSize + 2) * sizeof(uint64_t) );
+        if(ret)
+        {
+            cerr<<"Error: Munmap failed\nErrorno: "<<errno<<endl;
+            return -1;
+        }
+        delete[] meta[i].columns;
+    }
 
     delete[] meta;
-    delete[] str;
     return 0;
 }
