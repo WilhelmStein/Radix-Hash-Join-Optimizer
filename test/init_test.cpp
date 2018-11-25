b47bfa3f3a169b4d22c55e24e7709b9947ac331a
WilhelmStein <johnfreeman16561@gmail.com>
2018-11-25 17:42:09 +0200

    Reworked loading into memory

--- test/init_test.cpp
+++ test/init_test.cpp
@@ -1,95 +1,84 @@
 #include <iostream>
 #include <cstdint>
+#include <cstdio>
+#include <unistd.h>
 #include <cstring>
 #include <climits>
+#include <sys/mman.h>
 #include <fstream>
 #include <relation.hpp>
-#include <vector>
 
 using std::cout;
 using std::endl;
 using std::cin;
 using std::cerr;
 
-using std::vector;
-
 using RHJ::Relation;
 
-struct Table {
-    Relation r;
-    Relation::Tuple** index;
-    uint64_t rows, columns;
+struct Meta
+{
+    std::size_t rowSize, columnSize;
 
-    Table(uint64_t rows, uint64_t columns): rows(rows),
-                                            columns(columns),
-                                            r( (size_t) (rows * columns), "L" ) 
-                                            { index = new Relation::Tuple*[columns]; }
-    ~Table() { delete[] index; }
-};
+    RHJ::Relation::Tuple * columns;
+} * meta;
 
 int main()
 {
-    vector<Table> relations;
-    relations.reserve(40);
     char* str = new char[PATH_MAX + 1];
+    size_t inputSize = 0;
 
-    do {
-        //get each file path and load it to memory
+    while(true) {
         cin.getline(str, PATH_MAX);
-
         if (!strcmp(str, "Done"))
             break;
+        inputSize++;
+    }
 
-        std::ifstream inputFile(str, std::ios::in | std::ios::binary);
+    cin.seekg(0);
+    
+    meta = new Meta[inputSize];
 
-        if(!inputFile.is_open()){
-            cerr<<"Could not open file\n";
+    size_t currentFileNo = 0;
+
+    do {
+        // Get each file path
+        cin.getline(str, PATH_MAX);
+        
+
+        FILE* inputFile = nullptr;
+
+        if( !( inputFile = fopen(str, "r") ) ){
+            cerr<<"Could not open file "<<str<<"\nErrorno: "<<errno<<endl;
             return -1;
         }
 
-        // get length of file
-        inputFile.seekg(0, inputFile.end);
-        size_t buff_size = inputFile.tellg();
-        inputFile.seekg(0, inputFile.beg);
+        // Get length of file
+        fseek(inputFile, 0, SEEK_END);
+        size_t file_size = (size_t) ftell(inputFile);
+        fseek(inputFile, 0, SEEK_END);
 
-        //load whole file in memory
-        char* buffer = new char[buff_size + 1];
-        inputFile.read( buffer, buff_size); // H read isws na skaei an einai megalo to file
-        inputFile.close();
+        // Load whole file in memory
+        void* p = mmap(NULL, file_size, PROT_READ | PROT_WRITE, MAP_SHARED, fileno(inputFile), 0);
+        fclose(inputFile);
 
         uint64_t tuples(0UL), columns(0UL);
-        void* p = (void*) buffer;
 
-        //copy metadata
+        // Copy Metadata
         memmove( (void *) &tuples, p, sizeof(uint64_t));
-        p += sizeof(uint64_t);
+        p = (char*)p + sizeof(uint64_t);
         memmove( (void *) &columns, p, sizeof(uint64_t));
-        p += sizeof(uint64_t);
+        p = (char*)p + sizeof(uint64_t);
 
-        //create relation
-        //Table* tmp = new Table(tuples, columns);
-        relations.emplace_back(tuples, columns);   
-        
+        // Create Index        
         for(uint64_t j = 0UL; j < columns; j++) {
-            relations[relations.size() - 1].index[j] = &(relations[relations.size() - 1].r.tuples[j*tuples]);
-            for(uint64_t i = 0UL; i < tuples; i++) {
-                relations[relations.size() - 1].r.tuples[j*tuples + i].key = i;
-                memmove(&relations[relations.size() - 1].r.tuples[j*tuples + i].payload, p, sizeof(uint64_t) );
-                p += sizeof(uint64_t);
-            }
+            memmove(&meta[currentFileNo].columns[j], (void*) ((char*)p + j*tuples), sizeof(uint64_t));
         }
         
-        delete[] buffer;
+        currentFileNo++;
     } while( true );
-    
-    while(relations.size()){
-        #ifdef __VERBOSE__
-            cout<<relations[relations.size() - 1].r;
-        #endif
-        //delete relations[relations.size() - 1];
-        relations.pop_back();
-    }
 
+
+    delete[] meta;
     delete[] str;
     return 0;
 }
